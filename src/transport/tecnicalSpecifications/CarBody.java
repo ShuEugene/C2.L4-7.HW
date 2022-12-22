a59b7a307f097b7172f2726b52a5236f0ec9b37d
Евгений А. Шуликов <shueugene@hotmail.com>
2022-12-22 13:54:40 +0300

    C2.L4.HW5.1 is completed.

---
+++ src/transport/tecnicalSpecifications/CarBody.java
@@ -0,0 +1,23 @@
+package transport.tecnicalSpecifications;
+
+public enum CarBody {
+    SEDAN("«Седан»"),
+    HATCHBACK("«Хетчбэк»"),
+    COMPARTMENT("«Купе»"),
+    STATION_WAGON("«Универсал»"),
+    OFF_ROAD("«Внедорожник»"),
+    CROSSOVER("«Кроссовер»"),
+    PICKUP("«Пикап»"),
+    VAN("«Фургон»"),
+    MINIVAN("«Минивэн»");
+
+    private String title;
+
+    CarBody(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
